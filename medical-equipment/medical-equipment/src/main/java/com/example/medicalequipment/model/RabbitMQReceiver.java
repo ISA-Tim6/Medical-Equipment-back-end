0c230fb9c6b0e2de1aac953563599a425b10e30f
Anja Lovric <anja.lovric@gmail.com>
2024-01-28 00:08:08 +0100

    fix: web socket config fixed

--- medical-equipment/medical-equipment/src/main/java/com/example/medicalequipment/model/RabbitMQReceiver.java
+++ medical-equipment/medical-equipment/src/main/java/com/example/medicalequipment/model/RabbitMQReceiver.java
@@ -6,19 +6,27 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import com.example.medicalequipment.iservice.IContractCompanyService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class RabbitMQReceiver implements RabbitListenerConfigurer {
     private static final Logger logger = LoggerFactory.getLogger(RabbitMQReceiver.class);
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+	private SimpMessagingTemplate simpMessagingTemplate;
     @Override
     public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
     }
     @RabbitListener(queues = "${spring.rabbitmq.queue}")
-    public void receivedMessage(Location loc) {
+    public void receivedMessage(Location loc) throws MessagingException, JsonProcessingException {
         logger.info("User Details Received is.. " + loc.toString());
+        this.simpMessagingTemplate.convertAndSend("/socket-publisher", objectMapper.writeValueAsString(loc));
     }
     
     @Autowired
