fbf6cd7508428f81d6b224d9394722452b47ef99
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2024-01-28 19:32:03 +0100

    feat: Send delivery message scheduled

--- medical-equipment/medical-equipment/src/main/java/com/example/medicalequipment/model/RabbitMQSender.java
+++ medical-equipment/medical-equipment/src/main/java/com/example/medicalequipment/model/RabbitMQSender.java
@@ -1,5 +1,8 @@
 package com.example.medicalequipment.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,10 +11,13 @@ import org.springframework.stereotype.Service;
 
 import com.example.medicalequipment.iservice.ICompanyService;
 import com.example.medicalequipment.iservice.IContractCompanyService;
+import com.example.medicalequipment.repository.IContractRepository;
 
 @Service
 public class RabbitMQSender {
     private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private IContractRepository contractRepository;
     @Autowired
     public RabbitMQSender(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
@@ -31,11 +37,19 @@ public class RabbitMQSender {
         rabbitTemplate.convertAndSend(exchange,routingkey2, message);
     }
     
-    @Scheduled(cron = "0 0 0 1 * ?")//u ponoc prvog u mjesecu
-    //@Value("${spring.rabbitmq.routingkey2}")
-    private String routingkey23;
-    public void sendMessage3(String message){
-        rabbitTemplate.convertAndSend(exchange,routingkey2, message);
+    
+    @Scheduled(cron = "0 0 10 * * ?")	//svakog dana u 10h
+    public void sendDelivery(){
+    	int todayDayOfMonth = LocalDate.now().getDayOfMonth();
+    	List<ContractCompany> all = contractRepository.findAll();
+    	for(ContractCompany contract : all)
+    	{
+    		if(todayDayOfMonth == contract.getDayInMonth())
+    		{
+    			rabbitTemplate.convertAndSend(exchange, routingkey2, "Your equipment is successfully delivered.");
+    		}
+    	}
+        
     }
     
     
