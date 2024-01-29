c1bab6d8fe68bf6b6ccdc621bb6d93ce7d8ab9c9
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2024-01-29 19:31:41 +0100

    fix: new coordinates are added

--- simulator/src/main/java/rs/ac/uns/ftn/informatika/rabbitmq/RabbitMQSender.java
+++ simulator/src/main/java/rs/ac/uns/ftn/informatika/rabbitmq/RabbitMQSender.java
@@ -1,5 +1,6 @@
 package rs.ac.uns.ftn.informatika.rabbitmq;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -14,11 +15,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 @Service
 public class RabbitMQSender {
     private RabbitTemplate rabbitTemplate;
-    private CoordinateSenderService service;
    
-    public RabbitMQSender(RabbitTemplate rabbitTemplate, CoordinateSenderService s) {
+    public RabbitMQSender(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
-        this.service = s;
     }
     @Value("${spring.rabbitmq.exchange}")
     private String exchange;
@@ -26,7 +25,15 @@ public class RabbitMQSender {
     @Value("${spring.rabbitmq.routingkey}")
     private String routingkey;
     public void send(String timeInterval) throws JsonProcessingException, ExecutionException, InterruptedException{
-    	List<Producer> coords = this.service.sendCoordinate(45, 45, 0);
+    	List<Producer> coords = new ArrayList<>();
+    	coords.add(new Producer(45.244028564841, 19.85092364251614));
+    	coords.add(new Producer(45.24436849619236, 19.85069297254086));
+    	coords.add(new Producer(45.24466687870223, 19.850494489073757));
+    	coords.add(new Producer(45.244912382123715, 19.850338920950893));
+    	coords.add(new Producer(45.245138999724944, 19.850188717246056));
+    	coords.add(new Producer(45.24543737818778, 19.849990233778957));
+    	coords.add(new Producer(45.24579996256492, 19.849775657057766));
+    	coords.add(new Producer(45.24576219346693, 19.84942696988583));
     	for(Producer p: coords)
     	{
     		rabbitTemplate.convertAndSend(exchange,routingkey, p);
